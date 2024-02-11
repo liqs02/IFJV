580411b8a7472a12ff2720608bff93990ef7ab76
Blackky <patryklikus@icloud.com>
2024-02-11 19:08:30 +0100

    update

--- src/test/java/com/patryklikus/ifjv/schemas/SchemaExtractorImplTest.java
+++ src/test/java/com/patryklikus/ifjv/schemas/SchemaExtractorImplTest.java
@@ -2,13 +2,14 @@
 package com.patryklikus.ifjv.schemas;
 
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import com.patryklikus.ifjv.validators.JsonValidator;
 import com.patryklikus.ifjv.validators.JsonValidatorImpl;
-import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SchemaExtractorImplTest {
     private SchemaExtractor schemaExtractor;
@@ -38,17 +39,5 @@ class SchemaExtractorImplTest {
 
         String json = "{\"male\": true}";
         assertThrows(NoSuchElementException.class, () -> jsonValidator.validate(json.toCharArray(), schema).get());
-    }
-
-
-    @Test
-    void t2() {
-        int[] num = {1};
-        x(num);
-        System.out.println(num[0]);
-    }
-
-    void x(int[] num) {
-        num[0]++;
     }
 }
