292199604e73db791b27697e780140dfd1038200
Patrick <90937664+blackydev@users.noreply.github.com>
2024-02-17 12:07:09 +0100

    fix imports

--- src/main/java/com/patryklikus/ifjv/utils/GsonUtils.java
+++ src/main/java/com/patryklikus/ifjv/utils/GsonUtils.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+
 import javax.annotation.Nullable;
 
 public class GsonUtils {
--- src/test/java/com/patryklikus/ifjv/schemas/SchemaDeserializerImplTest.java
+++ src/test/java/com/patryklikus/ifjv/schemas/SchemaDeserializerImplTest.java
@@ -1,23 +1,24 @@
 /* Copyright Patryk Likus All Rights Reserved. */
 package com.patryklikus.ifjv.schemas;
 
-import static com.patryklikus.ifjv.schemas.SchemaDeserializerImplTestCases.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import com.patryklikus.ifjv.schemas.deserializers.SchemaDeserializer;
 import com.patryklikus.ifjv.schemas.deserializers.SchemaDeserializerImpl;
 import com.patryklikus.ifjv.schemas.models.*;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static com.patryklikus.ifjv.schemas.SchemaDeserializerImplTestCases.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DisplayName("SchemaExtractorImpl")
 class SchemaDeserializerImplTest {
